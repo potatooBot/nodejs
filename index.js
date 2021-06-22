c7d51b51d4fedbf0d42bb5cea5bcd8bf499a3287
Keshav Kumar <81036483+potatooBot@users.noreply.github.com>
2021-06-22 20:34:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// console.log("Hi node running");
+const fs= require("fs");
+// fs.writeFileSync("keshav.txt","This is my new file");
+// fs.appendFileSync("keshav.txt"," This is  something new ");
+// const buffer=fs.readFileSync("keshav.txt");
+// console.log(buffer.toString());
+fs.renameSync("sangita.txt","sangitaa.txt");
