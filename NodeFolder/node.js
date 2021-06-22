b456e422cde727eed0919db4e5461e992ff471a7
Keshav <kumarkeshav825@gmail.com>
2021-06-22 16:01:46 +0530

    "Async File"

---
+++ NodeFolder/node.js
@@ -0,0 +1,8 @@
+const fs=require("fs");
+// fs.writeFileSync("bio.txt","This is my bio");
+// fs.appendFileSync("bio.txt","\nThis is my bio 2");
+// let read =fs.readFileSync("bio.txt","utf-8");
+// console.log(read);
+// fs.renameSync("bio.txt","mybio.txt");
+// fs.unlinkSync("mybio.txt");
+// fs.rmdirSync("NodeFolder");
--- index.js
+++ index.js
@@ -4,4 +4,13 @@ const fs= require("fs");
 // fs.appendFileSync("keshav.txt"," This is  something new ");
 // const buffer=fs.readFileSync("keshav.txt");
 // console.log(buffer.toString());
-fs.renameSync("sangita.txt","sangitaa.txt");
+// fs.renameSync("sangita.txt","sangitaa.txt");
+// fs.writeFileSync("bio.txt","This is my Bio ");
+// fs.writeFile("fsAsync/new.txt","This is a new way",(err)=>{
+// console.log("file created");
+// });
+fs.readFile("fsAsync/new.txt","utf-8",(err,data)=>{
+    console.log(data);
+})
+
+
