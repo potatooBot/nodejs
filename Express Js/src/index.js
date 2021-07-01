0dfb9eae04c0cf79bbeae399893ba56cbc40fe82
Keshav <kumarkeshav825@gmail.com>
2021-07-01 23:57:43 +0530

    "template engine"

--- Express Js/src/index.js
+++ Express Js/src/index.js
@@ -1,34 +1,50 @@
-var express = require('express');
-const fs=require('fs');
-var path = require('path');
+// var express = require('express');
+// const fs=require('fs');
+// var path = require('path');
 
- const app = express();
-//  console.log(__dirname);
-// console.log(path.join(__dirname,"../public"))
-const static=path.join(__dirname,"../public")
-app.use(express.static(static));
+//  const app = express();
+// //  console.log(__dirname);
+// // console.log(path.join(__dirname,"../public"))
+// const static=path.join(__dirname,"../public")
 
-//  app.use(express.logger);
- app.get('/',function(req, res){
-     res.send(`<h1>Hi this is from express yipeee</h1>`);
- })
+// app.use(express.static(static));
+// //  app.use(express.logger);
+//  app.get('/',function(req, res){
+//      res.send(`<h1>Hi this is from express yipeee</h1>`);
+  
+//     // res.end();
 
-
- app.get("/about",(req, res)=>{
-    res.sendFile(path.join(__dirname,'/public/contactt.html'));  
-})
-
-
- const port =5500;
+    
+//  })
 
 
+//  app.get("/about",(req, res)=>{
+//     app.use("../public/contact.html",express.static(static));
+//     // res.sendFile(static,'+contact.html'); 
+//  })
 
 
-app.listen(port,()=>{
-console.log(`Listening is ${port}`);
- }); 
+//  const port =5500;
+
+
+
+
+// app.listen(port,()=>{
+// console.log(`Listening is ${port}`);
+//  }); 
  
 
-
-
+const express =require('express');
+const path = require('path');
+const app =express();
+// console.log(path.join(__dirname,"../public"));
+const staticPath=path.join(__dirname,"../public");
+app.use("/",express.static(staticPath));
+// app.get("/",(req, res)=>{
+ 
+// })
+const port =5500;
+app.listen(port,()=>{
+console.log("listening");
+});
 
