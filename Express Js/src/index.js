3e73060f74ebb4a02bea21551125cd710266c844
Keshav <kumarkeshav825@gmail.com>
2021-07-03 14:16:17 +0530

    Partials Added

--- Express Js/src/index.js
+++ Express Js/src/index.js
@@ -30,18 +30,39 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const hbs =require('hbs');
+
+
+const staticPath = path.join(__dirname,"../public");
+const templatePath=path.join(__dirname,"./templates");
+const partialsPath =path.join(__dirname,"./partials");
+// const viewsPath=path.join(__dirname,"../views");
 app.set("view engine", "hbs");
-const staticPath = path.join(__dirname, "../public");
+app.set("views",templatePath);
+hbs.registerPartials(partialsPath);
+app.locals.title = 'My App'
+app.locals.email="kumarkeshav825@gmail.com";
 
-// app.set("/",path.join(__dirname,"/views"));
+console.dir(app.locals.title);
+app.get("views");
+console.dir(app.locals.email);
+// console.dir(app.locals.strftime);
 
-// app.use("/", express.static(staticPath));
+
+// app.set("/",path.join("views",viewsPath));
+
+
 app.use(express.static(staticPath));
 app.get("", (req, res) => {
-  res.render("index", {
-    myName: "Keshav",
+  res.render("index",{ 
+     myName : "Keshav",
   });
 });
+app.get("/contact", (req, res) => {
+  res.render("contact",{
+    myName : "Amit",
+  });
+})
 
 const port = 5500;
 app.listen(port, () => {
